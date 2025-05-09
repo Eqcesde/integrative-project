a20de8424ce949ff00ce74ee4aeb031d63e07ad6
Enderson Quiroz <endequirozca@cesde.net>
2025-05-09 15:02:53 -0500

    se actualiza M2 Actividad 1

--- pages/1_📌_M2 Actvidad 1.py
+++ pages/1_📌_M2 Actvidad 1.py
@@ -139,19 +139,19 @@ st.dataframe(df_personas)
 st.write("## Datos desde CSV")
 st.dataframe(df_csv)
 
-st.write("Datos desde Excel")
+st.header("Datos desde Excel")
 df = cargar_datos()
 st.dataframe(df)
 
-st.write("Datos de Usuarios desde JSON")
+st.header("Datos de Usuarios desde JSON")
 st.dataframe(df_json)
 
-st.write("Datos desde URL")
+st.header("Datos desde URL")
 st.dataframe(df_url)
 
-st.write("Datos desde SQLite")
+st.header("Datos desde SQLite")
 st.dataframe(df_sql)
 
-st.write("Datos desde NumPy")
+st.header("Datos desde NumPy")
 st.dataframe(df_np)
 
